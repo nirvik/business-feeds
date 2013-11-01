8662fa6714546eaccbf70cef76bef3ee524e6453
nirvik1993@gmail.com <nirvik1993@gmail.com>
2013-11-01 18:32:58 +0530

    showing more titles

--- news.go
+++ news.go
@@ -14,7 +14,7 @@ var url string = "http://news.oneindia.in/rss/news-business-fb.xml"
 
 type entertainment struct{
 
-	Title string `xml:"channel>title"`
+	Title []string `xml:"channel>item>title"`
 	Description []string `xml:"channel>item>description"`
 
 }
@@ -24,14 +24,13 @@ type entertainment struct{
 //getting entertainment news
 func getNews(feed string) (n *entertainment,err error){
 
-	res,err:=http.Get(feed)
+	res,err:=http.Get(feed) 
 	
 	defer res.Body.Close()
 	if err!=nil{
 		return nil,err
 	}
-
-	b,err:=ioutil.ReadAll(res.Body)
+	b,err:=ioutil.ReadAll(res.Body) //read the binaries of the object
 	
 	if err!=nil{
 		return nil,err
@@ -39,8 +38,8 @@ func getNews(feed string) (n *entertainment,err error){
 
 	gossip:=new(entertainment)
 	
-	//err=xml.Unmarshal(b,&gossip)
-	err = xml.NewDecoder(bytes.NewBuffer(b)).Decode(&gossip)
+
+	err = xml.NewDecoder(bytes.NewBuffer(b)).Decode(&gossip) //Decode and put it in Gossip 
 
 	return gossip,err
 }
@@ -49,11 +48,13 @@ func getNews(feed string) (n *entertainment,err error){
 func main(){
 	gossip,err := getNews(url)
 	if err!=nil{
-		log.Fatalf("Log: %v", err)
+		log.Fatalf("Log: ", err)
 		return 
 	}
 
-	fmt.Println(gossip.Title)
+	for _,v:= range gossip.Title {
+		fmt.Println("\n",v)
+	}
 	for _,v:=range gossip.Description {
 		fmt.Println("\n",v)
 	}
